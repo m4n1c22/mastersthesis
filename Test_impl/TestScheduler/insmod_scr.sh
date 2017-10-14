c1b149e5d545e5d4635f6edc9033d6c56b5ddd38
m4n1c22 <sreeram.sadasivam@gmail.com>
2017-10-14 23:44:01 +0200

    new version of loadable scheduler added.

--- Test_impl/TestScheduler/insmod_scr.sh
+++
@@ -1,4 +0,0 @@
-sudo insmod process_queue.ko
-sudo insmod process_scheduler.ko time_quantum=5
-sudo insmod process_set.ko
-
--- Test_impl/TestScheduler/rmmod_scr.sh
+++
@@ -1,3 +0,0 @@
-sudo rmmod process_set.ko
-sudo rmmod process_scheduler.ko
-sudo rmmod process_queue.ko
